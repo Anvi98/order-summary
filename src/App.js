e412c971d41314dac7cd56566f12649fd7723598
Anvi Alex <epononanvialex@gmail.com>
2024-08-05 13:47:37 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import SummaryCard from './SummaryCard';
 import backgroundM from './images/pattern-background-mobile.svg';
 import backgroundD from './images/pattern-background-desktop.svg';
+
 function App() {
   return (
-    <div className=" w-full h-screen bg-cover flex justify-center items-center bg-pale-blue">
-      <img src={backgroundM} alt="" className='block absolute top-0 sm:hidden'/>
-      <img src={backgroundD} alt="" className='hidden absolute top-0 sm:block'/>
-      <SummaryCard/>
+    <div className="w-full h-screen flex justify-center items-center bg-pale-blue relative">
+      <img src={backgroundM} alt="" className="w-full h-full object-cover block absolute top-0 sm:hidden" />
+      <img src={backgroundD} alt="" className="w-full h-full object-cover hidden absolute top-0 sm:block" />
+      <SummaryCard />
     </div>
   );
 }
 
 export default App;
+
